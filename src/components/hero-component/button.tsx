d77313fd09762d5c1889675702db3c9fb9ffffdf
Андрей Крамарчук <inf1nitiveit@gmail.com>
2024-12-19 22:36:39 +0300

    Вносит первые наработки по созданию главного контейнера страницы

--- src/components/hero-component/button.tsx
+++ src/components/hero-component/button.tsx
@@ -1,6 +1,10 @@
 function Button() {
   return(
-    <button>Get in touch</button>
+    <button className="flex items-center justify-center px-4 py-2 bg-[#7E3AF2] text-white rounded-[50px] w-[188px] h-[50px]">
+      <span className="text-[16px] font-semibold leading-[24px] mr-3 ">Get in touch</span>
+      <img src="/public/img/button-icon.svg" alt="Icon" className="w-[24px] h-[24px]" />
+
+    </button>
   );
 }
 
--- src/components/hero-component/main-info.tsx
+++ src/components/hero-component/main-info.tsx
@@ -2,10 +2,17 @@ import Button from './button';
 
 function MainInfo() {
   return(
-    <div>
-      <h1>Hey, I’m Nick</h1>
-      <p>Frontend developer</p>
-      <p>Help you to create high-quality digital products that your clients will love and let your business thrive </p>
+    <div className='pl-[85px]'>
+      <h1 className='text-[60px] font-bold leading-[90px] text-[#27272A] mt-[112px] '>Hey, I’m Nick</h1>
+      <p className='text-[20px] leading-[30px] text-[#696969] mb-[92px]'>Frontend developer</p>
+
+      <div className="relative">
+        <div className="absolute top-0 left-0 w-8 h-1 bg-purple-500 "></div>
+        <p className="text-[20px] leading-[30px] text-[#696969] pt-[24px] mb-[60px] ">
+          Help you to create high-quality digital products that your clients will love and let your business thrive
+        </p>
+      </div>
+
       <Button />
     </div>
   );
